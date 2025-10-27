3efb6b7cb065b5366ff6bb2586abacb7375aca69
Ruben Dario Higuita Rubio <higuitaruben@hotmail.com>
2025-10-27 21:04:45 +0100

    Creaciones de paquetes contenido

--- src/ruben/play/Main.java
+++ src/ruben/play/Main.java
@@ -2,14 +2,30 @@ package ruben.play;
 
 import java.util.Scanner;
 
+import ruben.play.contenido.Pelicula;
+import ruben.play.plataforma.Usuario;
+
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hola Mundo!");
+		System.out.println("RUBEN PLAY");
 		
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Cual es tu nombre?");
-		String nombre = scanner.nextLine();
+		Pelicula pelicula = new Pelicula();
+		pelicula.titulo = "El señor de los anillos";
+		pelicula.anioEstreno = 2002;
+		pelicula.genero = "Fantasía";
+		pelicula.calificar(4.2);
 		
-		System.out.println("Hola " + nombre  + ", Esto es ruben.play");
+		Usuario usuario = new Usuario();
+		usuario.nombre = "Carlos";
+		
+		usuario.ver(pelicula);
+
+//		
+//		Scanner scanner = new Scanner(System.in);
+//		System.out.println("Cual es tu nombre?");
+//		String nombre = scanner.nextLine();
+//		
+//		System.out.println("Hola " + nombre  + ", Esto es ruben.play");
 	}
 }
+
